c38a577969b49eb32d0615fdb6d63740f51ee853
Jake Atwell <jatwell@alexinc.com>
2022-09-01 17:34:02 -0400

    Encapsulate type code and delegate get_price_code to Price class

--- movie.py
+++ movie.py
@@ -1,17 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Price(ABC):
+    @abstractmethod
+    def get_price_code(self):
+        pass
+
+
+class ChildrensPrice(Price):
+    def get_price_code(self):
+        return Movie.CHILDRENS
+
+
+class NewReleasePrice(Price):
+    def get_price_code(self):
+        return Movie.NEW_RELEASE
+
+
+class RegularPrice(Price):
+    def get_price_code(self):
+        return Movie.REGULAR
+
+
 class Movie:
+    CHILDRENS = 2
+    REGULAR = 0
+    NEW_RELEASE = 1
+
     def __init__(self, title, price_code):
         # NOTE: note sure how to access these class constants without instantiating object
-        self.CHILDRENS = 2
-        self.REGULAR = 0
-        self.NEW_RELEASE = 1
         self._title = title
-        self._price_code = price_code
+        self.set_price_code(price_code)
 
     def get_price_code(self):
-        return self._price_code
+        return self._price.get_price_code()
 
     def set_price_code(self, arg):
-        self._price_code = arg
+        if arg == self.REGULAR:
+            self._price = RegularPrice()
+        elif arg == self.CHILDRENS:
+            self._price = ChildrensPrice()
+        elif arg == self.NEW_RELEASE:
+            self._price = NewReleasePrice()
+        else:
+            raise NotImplementedError
 
     def get_title(self):
         return self._title
