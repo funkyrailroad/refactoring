1b7bbe052cfa2620a89d1456332f625fd0773094
Jake Atwell <jatwell@alexinc.com>
2022-09-07 15:02:41 -0400

    Move get_charge to Price class

--- movie.py
+++ movie.py
@@ -5,6 +5,22 @@ class Price(ABC):
     def get_price_code(self):
         pass
 
+    def get_charge(self, days_rented):
+        this_amount = 0
+
+        # determine amounts for self line
+        if self.get_price_code() == Movie.REGULAR:
+            this_amount +=  2
+            if days_rented > 2:
+                this_amount += (days_rented - 2 ) * 1.5
+        if self.get_price_code() == Movie.NEW_RELEASE:
+            this_amount += days_rented * 3
+        if self.get_price_code() == Movie.CHILDRENS:
+            this_amount +=  1.5
+            if days_rented > 3:
+                this_amount += (days_rented - 3 ) * 1.5
+        return this_amount
+
 
 class ChildrensPrice(Price):
     def get_price_code(self):
@@ -48,20 +64,7 @@ class Movie:
         return self._title
 
     def get_charge(self, days_rented):
-        this_amount = 0
-
-        # determine amounts for self line
-        if self.get_price_code() == self.REGULAR:
-            this_amount +=  2
-            if days_rented > 2:
-                this_amount += (days_rented - 2 ) * 1.5
-        if self.get_price_code() == self.NEW_RELEASE:
-            this_amount += days_rented * 3
-        if self.get_price_code() == self.CHILDRENS:
-            this_amount +=  1.5
-            if days_rented > 3:
-                this_amount += (days_rented - 3 ) * 1.5
-        return this_amount
+        return self._price.get_charge(days_rented)
 
     def get_frequent_renter_points(self, days_rented):
         # add frequent renter points
